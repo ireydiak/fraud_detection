f4008d4347048f07ec8542820f028eb657d6526b
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-04-15 18:20:47 -0400

    safe commit

--- TP2/JC/models.py
+++ TP2/JC/models.py
@@ -87,12 +87,13 @@ class MLP2(nn.Module):
 
 
 class MLPTrainer:
-    def __init__(self, model, optimizer, device: str = "cuda"):
+    def __init__(self, model, optimizer, device: str = "cuda", verbose: bool = True):
         self.model = model.to(device)
         self.device = device
         self.optimizer = optimizer
         self.loss_fn = nn.BCEWithLogitsLoss()
         self.metric_values = dict(train_loss=[])
+        self.verbose = verbose
 
     def evaluate(self, y_true, y_pred, pos_label=0) -> dict:
         res = {"Precision": -1, "Recall": -1, "F1-Score": -1}
@@ -109,11 +110,12 @@ class MLPTrainer:
         y_true, y_pred = [], []
         with torch.no_grad():
             for row in dataset:
-                X, y = row[:, :row.shape[1] - 1], row[:, row.shape[1] - 1]
+                X, y = row
                 X, y = X.to(self.device).float(), y.float()
 
-                scores = self.model(X)
-                scores = torch.round(torch.sigmoid(scores))
+                logits = self.model(X)
+                scores = torch.sigmoid(logits)
+                # logits / (logits + torch.abs(logits))
                 scores[scores >= 0.5] = 1
                 scores[scores < 0.5] = 0
                 y_true.extend(y.tolist())
@@ -129,12 +131,12 @@ class MLPTrainer:
             print("Epoch: {} of {}".format(epoch + 1, num_epochs))
             train_loss = 0.0
 
-            with tqdm(range(len(train_loader))) as t:
+            with tqdm(range(len(train_loader)), disable=not self.verbose) as t:
                 train_losses = []
                 for i, row in enumerate(train_loader, 0):
                     # transfer tensors to selected device
-                    train_inputs = row[:, :row.shape[1] - 1].to(self.device, dtype=torch.float).float()
-                    train_labels = row[:, row.shape[1] - 1].to(self.device, dtype=torch.long).float()
+                    train_inputs = row[0].to(self.device, dtype=torch.float).float()
+                    train_labels = row[1].to(self.device, dtype=torch.long).float()
                     # zero the parameter gradients
                     self.optimizer.zero_grad()
 
@@ -178,24 +180,30 @@ class InstaCartDataset(Dataset):
         return self.X[index], self.y[index]
 
     def loaders(self, num_workers: int = 4) -> Tuple[DataLoader, DataLoader]:
-        train_set, train_set = self.train_test_split(self.test_ratio)
+        train_set, test_set = self.train_test_split(self.test_ratio)
         train_ldr = DataLoader(dataset=train_set, batch_size=self.batch_size, num_workers=num_workers)
-        test_ldr = DataLoader(dataset=train_set, batch_size=self.batch_size, num_workers=num_workers)
+        test_ldr = DataLoader(dataset=test_set, batch_size=self.batch_size, num_workers=num_workers)
         return train_ldr, test_ldr
 
     def train_test_split(self, test_ratio: float):
         train_ratio = 1 - test_ratio
         train_idx, test_idx = [], []
 
-        for label, ratio in self.class_ratio.items():
-            # Fetch and shuffle indices of a single class
-            label_data_idx = np.where(self.y == label)[0]
-            shuffled_idx = torch.randperm(len(label_data_idx)).long()
+        if self.class_ratio:
+            for label, ratio in self.class_ratio.items():
+                # Fetch and shuffle indices of a single class
+                label_data_idx = np.where(self.y == label)[0]
+                shuffled_idx = torch.randperm(len(label_data_idx)).long()
 
-            # Generate training and test sets
-            n_train_sample = int(len(label_data_idx) * train_ratio * ratio)
-            train_idx.extend(label_data_idx[shuffled_idx[n_train_sample:]])
-            test_idx.extend(label_data_idx[shuffled_idx[:n_train_sample]])
+                # Generate training and test sets
+                n_train_sample = int(len(label_data_idx) * train_ratio * ratio)
+                train_idx.extend(label_data_idx[shuffled_idx[:n_train_sample]])
+                test_idx.extend(label_data_idx[shuffled_idx[n_train_sample:]])
+        else:
+            shuffled_idx = torch.randperm(len(self.X)).long()
+            n_train_sample = int(len(self.X) * train_ratio)
+            train_idx.extend(shuffled_idx[:n_train_sample])
+            test_idx.extend(shuffled_idx[n_train_sample:])
 
         train_set = Subset(self, train_idx)
         test_set = Subset(self, test_idx)
@@ -204,31 +212,45 @@ class InstaCartDataset(Dataset):
 
 
 def main():
+    available_models = [MLP5, MLP3, MLP2]
     df = pd.read_csv("./data/train_orders.csv")
-    data = df.to_numpy()
-    cls_0_ratio = (data[:, -1] == 0).sum() / len(data)
-    cls_1_ratio = (data[:, -1] == 1).sum() / len(data)
-    class_ratio = {0: cls_0_ratio, 1: cls_1_ratio}
-    dataset = InstaCartDataset(data, class_ratio=class_ratio)
+    X = df.to_numpy()
+    cls_0_ratio = (X[:, -1] == 0).sum() / len(X)
+    cls_1_ratio = (X[:, -1] == 1).sum() / len(X)
+    class_ratio = None # {0: cls_0_ratio, 1: cls_1_ratio}
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    n_epochs = 200
-    train_ldr, test_ldr = dataset.loaders()
-    results = []
+    n_epochs = 1
+    n_runs = 1
 
-    for model_cls in [MLP5, MLP3, MLP2]:
-        model = model_cls(in_features=dataset.in_features)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    trainer = MLPTrainer(model=model, optimizer=optimizer, device=device)
-    trainer.train(train_ldr, n_epochs)
-    y_true, y_pred = trainer.test(test_ldr)
-    metrics = trainer.evaluate(y_true, y_pred)
-    print("Results for %s" % model_cls.name)
-    print(metrics)
-    results.append([metrics["Precision"], metrics["Recall"], metrics["F1-Score"]])
+    available_metrics = ["Precision", "Recall", "F1-Score"]
+    results = {model.name: {metric_name: [] for metric_name in available_metrics} for model in available_models}
+
+    for model_cls in available_models:
+        for run in range(n_runs):
+            # Partition des données aléatoires à chaque itération
+            dataset = InstaCartDataset(X, class_ratio=class_ratio)
+            train_ldr, test_ldr = dataset.loaders()
+            # Entraînement
+            model = model_cls(in_features=dataset.in_features)
+            optimizer = optim.Adam(model.parameters(), lr=1e-3)
+            trainer = MLPTrainer(model=model, optimizer=optimizer, device=device)
+            trainer.train(train_ldr, n_epochs)
+            # Évaluation
+            y_true, y_pred = trainer.test(test_ldr)
+            run_results = trainer.evaluate(y_true, y_pred)
+            print("Results for %s" % model_cls.name)
+            print(run_results)
+            for metric_name in available_metrics:
+                results[model_cls.name][metric_name].append(run_results[metric_name])
+
+    full_results = []
+    for model_results in results.values():
+        full_results.append(["{:2.4f} ({:2.4f})".format(np.mean(res), np.std(res)) for res in model_results.values()])
+
     summary_df = pd.DataFrame(
         results,
-        columns=metrics.keys(),
-        index=[x.name for x in [MLP5, MLP3, MLP2]]
+        columns=available_metrics,
+        index=[model.name for model in available_models]
     )
     summary_df.to_csv("results/instacart_results.csv")
     print(summary_df)
