d224ad3735c99211017ebed5687a05071c11cb8e
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-04-17 12:05:38 -0400

    ajout de texte explicatif et des métriques precision, recall et f1-score personnalisées.

---
+++ TP2/JC/metrics.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def precision(y_true: np.ndarray, y_pred: np.ndarray, pos_label: int = 0):
+    tp = len([a for a, p in zip(y_true, y_pred) if a == p and p == pos_label])
+    fp = len([a for a, p in zip(y_true, y_pred) if a != p and p == pos_label])
+    return tp / (tp + fp)
+
+
+def recall(y_true: np.ndarray, y_pred: np.ndarray, pos_label: int = 0):
+    tp = len([a for a, p in zip(y_true, y_pred) if a == p and p == pos_label])
+    fn = len([a for a, p in zip(y_true, y_pred) if a != p and p == int(not bool(pos_label))])
+    return tp / (tp + fn)
+
+
+def f1score(y_true: np.ndarray, y_pred: np.ndarray, pos_label: int = 0):
+    prec = precision(y_true, y_pred, pos_label)
+    rec = recall(y_true, y_pred, pos_label)
+    return (2 * prec * rec) / (rec + prec)
+
+
+def precision_recall_f1(y_true: np.ndarray, y_pred: np.ndarray, pos_label: int = 0):
+    return precision(y_true, y_pred, pos_label), recall(y_true, y_pred, pos_label), f1score(y_true, y_pred, pos_label)
