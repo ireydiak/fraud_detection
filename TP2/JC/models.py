a3a6dab183491247e4e89165f3cf762a496a340e
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-04-16 21:44:51 -0400

    safe commit

--- TP2/JC/models.py
+++ TP2/JC/models.py
@@ -12,6 +12,68 @@ from collections import defaultdict
 from sklearn import metrics as sk_metrics
 from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler, Sampler, Subset
 
+from torch import nn, optim
+from tqdm import tqdm
+import sklearn.metrics as sk_metrics
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, Subset, DataLoader
+from torch.utils.data.dataset import T_co
+from typing import Tuple
+from sklearn.model_selection import train_test_split
+
+
+class InstaCartDataset(Dataset):
+    def __init__(self, data: np.array, val_ratio: float = 0.1, test_ratio: float = 0.33, batch_size: int = 32,
+                 class_ratio=None):
+        self.name = self.__class__.__name__
+        # self.X = data[:, :-1]
+        # self.y = data[:, -1]
+        self.X = data
+        self.val_ratio = val_ratio
+        self.test_ratio = test_ratio
+        self.batch_size = batch_size
+
+        self.n_instances = self.X.shape[0]
+        self.in_features = self.X.shape[1]
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, index) -> T_co:
+        return self.X[index], self.y[index]
+
+    def loaders(self, num_workers: int = 4, seed: int = None) -> Tuple[DataLoader, DataLoader]:
+        X_train, X_test = train_test_split(self.X, test_size=self.test_ratio)
+        train_ldr = DataLoader(dataset=X_train, batch_size=self.batch_size, num_workers=num_workers)
+        test_ldr = DataLoader(dataset=X_test, batch_size=self.batch_size, num_workers=num_workers)
+        return train_ldr, test_ldr
+
+    def split_train_test(self, test_pct: float = .5, label: int = 0, seed=None) -> Tuple[Subset, Subset]:
+        assert (label == 0 or label == 1)
+
+        if seed:
+            torch.manual_seed(seed)
+
+        # Fetch and shuffle indices of a single class
+        label_data_idx = np.where(self.y == label)[0]
+        shuffled_idx = torch.randperm(len(label_data_idx)).long()
+
+        # Generate training set
+        num_test_sample = int(len(label_data_idx) * test_pct)
+        num_train_sample = int(len(label_data_idx) * (1. - test_pct))
+        train_set = Subset(self, label_data_idx[shuffled_idx[num_train_sample:]])
+
+        # Generate test set based on the remaining data and the previously filtered out labels
+        remaining_idx = np.concatenate([
+            label_data_idx[shuffled_idx[:num_test_sample]],
+            np.where(self.y == int(not label))[0]
+        ])
+        test_set = Subset(self, remaining_idx)
+
+        return train_set, test_set
+
 
 class MLP5(nn.Module):
     name = "MLP5"
@@ -88,13 +150,31 @@ class MLP2(nn.Module):
 
 
 class MLPTrainer:
-    def __init__(self, model, optimizer, device: str = "cuda", verbose: bool = True):
+    def __init__(self, model, optimizer, device: str = "cuda"):
         self.model = model.to(device)
         self.device = device
         self.optimizer = optimizer
         self.loss_fn = nn.BCEWithLogitsLoss()
-        self.metric_values = dict(train_loss=[])
-        self.verbose = verbose
+
+    def test(self, dataset):
+        self.model.eval()
+
+        y_true, y_pred = [], []
+        with torch.no_grad():
+            for row in dataset:
+                # X, y = row
+                # X, y = X.to(self.device).float(), y.float()
+                X = row[:, :-1].to(self.device).float()
+                y = row[:, -1].to(self.device).float()
+
+                logits = self.model(X)
+                scores = torch.sigmoid(logits)
+                # logits / (logits + torch.abs(logits))
+                scores[scores >= 0.7] = 1
+                scores[scores < 0.3] = 0
+                y_true.extend(y.tolist())
+                y_pred.extend(scores.cpu().squeeze(1).tolist())
+        return np.array(y_true, dtype=np.int8), np.array(y_pred, dtype=np.int8)
 
     def evaluate(self, y_true, y_pred, pos_label=0) -> dict:
         res = {"Precision": -1, "Recall": -1, "F1-Score": -1}
@@ -105,27 +185,13 @@ class MLPTrainer:
 
         return res
 
-    def test(self, dataset):
-        self.model.eval()
-
-        y_true, y_pred = [], []
-        with torch.no_grad():
-            for row in dataset:
-                X, y = row
-                X, y = X.to(self.device).float(), y.float()
-                # X = row[:, :-1].to(self.device, dtype=torch.float).float()
-                # y = row[:, -1].to(self.device, dtype=torch.long).float()
-
-                logits = self.model(X)
-                scores = torch.sigmoid(logits)
-                # logits / (logits + torch.abs(logits))
-                scores[scores >= 0.6] = 1
-                scores[scores < 0.4] = 0
-                y_true.extend(y.tolist())
-                y_pred.extend(scores.cpu().squeeze(1).tolist())
-        return y_true, y_pred
-
     def train(self, dataset_ldr, num_epochs):
+        """
+        Train the model for num_epochs times
+        Args:
+            num_epochs: number times to train the model
+        """
+
         # Create pytorch's train data_loader
         train_loader = dataset_ldr
 
@@ -133,22 +199,19 @@ class MLPTrainer:
         for epoch in range(num_epochs):
             print("Epoch: {} of {}".format(epoch + 1, num_epochs))
             train_loss = 0.0
-
-            with tqdm(range(len(train_loader)), disable=not self.verbose) as t:
+            with tqdm(range(len(train_loader))) as t:
                 train_losses = []
-                for i, row in enumerate(train_loader, 0):
+                for i, data in enumerate(train_loader, 0):
                     # transfer tensors to selected device
-                    train_inputs = row[0].to(self.device, dtype=torch.float).float()
-                    train_labels = row[1].to(self.device, dtype=torch.long).float()
-                    # train_inputs = row[:, :-1].to(self.device, dtype=torch.float).float()
-                    # train_labels = row[:, -1].to(self.device, dtype=torch.long).float()
+                    train_inputs = data[:, :-1].to(self.device).float()
+                    train_labels = data[:, -1].to(self.device).float().unsqueeze(1)
                     # zero the parameter gradients
                     self.optimizer.zero_grad()
 
                     # forward pass
                     train_outputs = self.model(train_inputs)
                     # compute loss
-                    loss = self.loss_fn(train_outputs, train_labels.unsqueeze(1))
+                    loss = self.loss_fn(train_outputs, train_labels)
 
                     # Use autograd to compute the backward pass.
                     loss.backward()
@@ -159,83 +222,23 @@ class MLPTrainer:
 
                     # print metrics along progress bar
                     train_loss += loss.item()
-                    t.set_postfix(loss="{:05.3f}".format(train_loss / (i + 1)))
+                    t.set_postfix(loss='{:05.3f}'.format(train_loss / (i + 1)))
                     t.update()
 
             # evaluate the model on validation data after each epoch
-            self.metric_values["train_loss"].append(np.mean(train_losses))
-
-
-class InstaCartDataset(Dataset):
-    def __init__(self, data: np.array, test_ratio: float = 0.30, batch_size: int = 32, class_ratio: dict = None):
-        self.name = self.__class__.__name__
-        self.X = data[:, :-1]
-        self.y = data[:, -1]
-        self.test_ratio = test_ratio
-        self.batch_size = batch_size
-        self.class_ratio = class_ratio
-        self.n_samples = self.X.shape[0]
-        self.n_instances = self.X.shape[0]
-        self.in_features = self.X.shape[1]
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, index) -> T_co:
-        return self.X[index], self.y[index]
-
-    def loaders(self, num_workers: int = 0) -> Tuple[DataLoader, DataLoader]:
-        train_set, test_set = self.train_test_split(self.test_ratio)
-        train_ldr = DataLoader(dataset=train_set, batch_size=self.batch_size, num_workers=num_workers)
-        test_ldr = DataLoader(dataset=test_set, batch_size=self.batch_size, num_workers=num_workers)
-        return train_ldr, test_ldr
-
-    def train_test_split(self, test_ratio: float):
-        train_ratio = 1 - test_ratio
-        train_idx, test_idx = [], []
-
-        # X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(self.X, self.y, test_size=test_ratio)
-        # train_set = np.concatenate(
-        #     (X_train, np.expand_dims(y_train, 1)),
-        #     axis=1
-        # )
-        # test_set = np.concatenate(
-        #     (X_test, np.expand_dims(y_test, 1)),
-        #     axis=1
-        # )
-
-        if self.class_ratio:
-            for label, ratio in self.class_ratio.items():
-                # Fetch and shuffle indices of a single class
-                label_data_idx = np.where(self.y == label)[0]
-                shuffled_idx = torch.randperm(len(label_data_idx)).long()
-
-                # Generate training and test sets
-                n_train_sample = int(len(label_data_idx) * train_ratio * ratio)
-                train_idx.extend(label_data_idx[shuffled_idx[:n_train_sample]])
-                test_idx.extend(label_data_idx[shuffled_idx[n_train_sample:]])
-        else:
-            shuffled_idx = torch.randperm(len(self.X)).long()
-            n_train_sample = int(len(self.X) * train_ratio)
-            train_idx.extend(shuffled_idx[:n_train_sample])
-            test_idx.extend(shuffled_idx[n_train_sample:])
-
-        train_set = Subset(self, train_idx)
-        test_set = Subset(self, test_idx)
-
-        return train_set, test_set
+            # self.metric_values['train_loss'].append(np.mean(train_losses))
 
 
 def main():
-    available_models = [MLP5, MLP3, MLP2]
+    available_models = [MLP5, MLP3]
     df = pd.read_csv("./data/train_orders.csv")
     X = df.to_numpy()
     cls_0_ratio = (X[:, -1] == 0).sum() / len(X)
     cls_1_ratio = (X[:, -1] == 1).sum() / len(X)
     class_ratio = {0: cls_0_ratio, 1: cls_1_ratio}
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    n_epochs = 10
-    n_runs = 1
+    n_epochs = 200
+    n_runs = 10
 
     available_metrics = ["Precision", "Recall", "F1-Score"]
     results = {model.name: {metric_name: [] for metric_name in available_metrics} for model in available_models}
@@ -246,7 +249,7 @@ def main():
             dataset = InstaCartDataset(X, test_ratio=0.5, class_ratio=class_ratio)
             train_ldr, test_ldr = dataset.loaders()
             # Entraînement
-            model = model_cls(in_features=dataset.in_features)
+            model = model_cls(in_features=dataset.in_features-1)
             optimizer = optim.Adam(model.parameters(), lr=1e-3)
             trainer = MLPTrainer(model=model, optimizer=optimizer, device=device)
             trainer.train(train_ldr, n_epochs)
